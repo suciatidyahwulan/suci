257fb7e1ababf29e0c1d2d6e3cd76f919ed0bbe6
suciatidyahwulan <shintaadji40@gmail.com>
2025-10-28 11:35:14 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 // 1. Pilih tombol berdasarkan ID-nya
-const tombolSapa = document.getElementById('sapa Button');
+const tombolSapa = document.getElementById('sapaButton');
 // 2. Tambahkan 'event listener' untuk aksi 'click'
 tombolsapa.addEventListener('click', function() {
     // 3. Jalankan kode ini ketika tombol di-klik
     alert('Halo! Terimakasih sudah berkunjung!');
-]);
+});
                                            
